f204d0c4d86bbe2109a57ff68d7655b23c997b6a
Vladislave Mungin <natalya.mungina9@mail.ru>
2023-08-11 16:09:58 +0300

    first commit

---
+++ js/modules/calc.js
@@ -0,0 +1,108 @@
+function calc(){
+    const result = document.querySelector('.calculating__result span');
+    let sex,height,weight,age,ratio;
+    if(localStorage.getItem('sex')){
+        sex=localStorage.getItem('sex');
+    } else {
+        sex='female';
+        localStorage.setItem('sex','female');
+    }
+    if(localStorage.getItem('ratio')){
+        ratio=localStorage.getItem('ratio');
+    } else {
+        ratio=1.375;
+        localStorage.setItem('ratio',1.375)
+    }
+
+    function setActiveClaass(selector,activeClass){
+        const elem = document.querySelectorAll(selector);
+        elem.forEach(el=>{
+            el.classList.remove(activeClass);
+            if(el.getAttribute('id')===localStorage.getItem('sex')){
+                el.classList.add(activeClass);
+            }
+            if(el.getAttribute('data-active')===localStorage.getItem('ratio')){
+                el.classList.add(activeClass)
+            }
+        })
+    }
+
+    setActiveClaass('#gender div', 'calculating__choose-item_active');
+    setActiveClaass('.calculating__choose_big div', 'calculating__choose-item_active');
+
+    function calcTotal(){
+        if(!sex||!height||!weight||!age||!ratio){
+            result.textContent='____';
+            return;
+        }
+
+        if(sex==='female'){
+            result.textContent=Math.round((447.6+(9.2*weight)+(3.1*height)-(4.3*age))*ratio);
+        } else{
+            result.textContent=Math.round((88.36+(13.4*weight)+(4.8*height)-(5.7*age))*ratio);  
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInfo(selector,activeClass){
+        const elem = document.querySelectorAll(selector);
+
+        elem.forEach(el=>{
+            el.addEventListener('click',(e)=>{
+                if(e.target.getAttribute('data-active')){
+                    ratio=+e.target.getAttribute('data-active');
+                    localStorage.setItem('ratio',+e.target.getAttribute('data-active'));
+                } else{
+                    sex=e.target.getAttribute('id');
+                    localStorage.setItem('sex',e.target.getAttribute('id'))
+                }
+                
+                elem.forEach(el=>{
+                    el.classList.remove(activeClass);
+    
+                })
+    
+                e.target.classList.add(activeClass);
+                calcTotal();
+            })
+        })
+        
+        
+    }
+
+    getStaticInfo('#gender div', 'calculating__choose-item_active');
+    getStaticInfo('.calculating__choose_big div', 'calculating__choose-item_active');
+
+    function getDinamicInfo(selector){
+        const input =document.querySelector(selector);
+
+        input.addEventListener('input',()=>{
+
+            if(input.value.match(/\D/g)){
+                input.style.border='1px solid red';
+            } else {
+                input.style.border='none';
+            }
+            switch(input.getAttribute('id')){
+                case 'height': 
+                    height=+input.value;
+                    break;
+                case 'weight':
+                    weight=+input.value;
+                    break;
+                case 'age':
+                    age=+input.value;
+                    break;
+            }
+            calcTotal();
+        })
+        
+    }
+
+    getDinamicInfo('#height');
+    getDinamicInfo('#weight');
+    getDinamicInfo('#age');
+}
+
+export default calc;
---
+++ js/script.js
@@ -0,0 +1,33 @@
+    import tabs from './modules/tabs';
+    import modal from './modules/modal';
+    import calc from './modules/calc';
+    import cards from './modules/cards';
+    import forms from './modules/forms';
+    import slider from './modules/slider';
+    import timer from './modules/timer';
+    import { openModal } from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', function() {
+    const modalTimerId = setTimeout(()=>openModal('.modal',modalTimerId), 1000);
+    const deadline = '2024-06-11';
+
+    tabs('.tabheader__item','.tabcontent','.tabheader__items','.tabheader__item_active');
+    modal('[data-modal]','.modal',modalTimerId);
+    calc();
+    cards();
+    forms('form',modalTimerId);
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+    timer('.timer',deadline);
+    
+    
+});
+
