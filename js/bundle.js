d32cf737871ea9793fe5c41e83764c2d0054a696
Vladislav Mungin <93925061+VladislavMungin@users.noreply.github.com>
2023-08-14 16:13:10 +0300

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -677,7 +677,7 @@ window.addEventListener('DOMContentLoaded', function() {
     const modalTimerId = setTimeout(()=>(0,_modules_modal__WEBPACK_IMPORTED_MODULE_1__.openModal)('.modal',modalTimerId), 1000);
     const deadline = '2024-06-11';
 
-    (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_0__["default"])('.tabheader__item','.tabcontent','.tabheader__items','.tabheader__item_active');
+    (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_0__["default"])('.tabheader__item','.tabcontent','.tabheader__items','tabheader__item_active');
     (0,_modules_modal__WEBPACK_IMPORTED_MODULE_1__["default"])('[data-modal]','.modal',modalTimerId);
     (0,_modules_calc__WEBPACK_IMPORTED_MODULE_2__["default"])();
     (0,_modules_cards__WEBPACK_IMPORTED_MODULE_3__["default"])();
